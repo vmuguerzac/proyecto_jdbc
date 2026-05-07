6a2166c0f5737f8dca400429c7e03f380194e120
vmuguerzac <vmuguerzac@gmail.com>
2026-05-06 22:18:03 -0500

    change: Agregado InsertarLibro y refactoring de la clase

--- src/ConexionDemo.java
+++ src/ConexionDemo.java
@@ -2,9 +2,9 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class ConexionDemo {
-    
+    static Scanner scanner = new Scanner(System.in); // Insrtanciar Scanner
+    static Connection conexion = null;
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in); // Insrtanciar Scanner
         String url = "jdbc:mysql://localhost:3306/pageturner";
         String usuario = "root";
         String pwd = "victor18";
@@ -31,14 +31,48 @@ public class ConexionDemo {
     }
 
     public static void VerificarConexion(String url, String usuario, String pwd){
-        try(Connection conexion = DriverManager.getConnection(url, usuario, pwd)){
-            System.out.println("Conexion OK: " + conexion.getCatalog());
+        try(Connection conn = DriverManager.getConnection(url, usuario, pwd)){
+            System.out.println("Conexion OK: " + conn.getCatalog());
+            conexion = conn;
         }catch(SQLException ex){
             System.out.println("Error: " + ex.getMessage());
         }
     }
 
     public static boolean InsertarLibro(){
+        // Crear el objeto libro vacio
+        Libro libro = new Libro();
+        System.out.println("Agregar Libro: ");
+        // Capturar datos del usuario
+        System.out.println("Ingrese el ISBN del libro: ");
+        libro.setIsbn(scanner.nextLine());
+        System.out.println("Ingrese el Titulo del libro: ");
+        libro.setTitulo(scanner.nextLine());
+        System.out.println("Ingrese el Autor del libro: ");
+        libro.setAutor(scanner.nextLine());
+        System.out.println("Ingrese el Precio del libro: ");
+        libro.setPrecio(scanner.nextDouble());
+        System.out.println("Ingrese el Stock del libro: ");
+        libro.setStock(scanner.nextInt());
+        // Insertar el libro en la DB
+        // preparar el query
+        String query = 
+            "INSERT INTO libros(isbn, titulo, autor, precio, stock) " +
+            "VALUES(?, ?, ?, ?, ?)";
+        // usar el preparedstatement con try catch con resources
+        try(PreparedStatement ps = conexion.prepareStatement(query)){
+            ps.setString(1, libro.getIsbn());
+            ps.setString(2, libro.getTitulo());
+            ps.setString(3, libro.getAutor());
+            ps.setDouble(4, libro.getPrecio());
+            ps.setInt(5, libro.getStock());
+            // Ejecutar
+            int filas = ps.executeUpdate(); // Ejecutar INSERT, UPDATE o DELETE
+            System.out.println("(" + filas + ")" + " Filas insertadas");
+            if(filas > 0) return true;
+        }catch(SQLException ex){
+            System.out.println("Hubo un error al agregar el libro: " + ex.getMessage());
+        }
         return false;
     }
 }
