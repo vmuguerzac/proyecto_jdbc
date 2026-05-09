a0939d7050f9fd17925ed1e79e24324071967f97
vmuguerzac <vmuguerzac@gmail.com>
2026-05-08 21:36:35 -0500

    Change: Modificado ListtarLibro() change: Agregado Actualizar y Eliminar Libro

--- src/ConexionDemo.java
+++ src/ConexionDemo.java
@@ -4,31 +4,47 @@ import java.util.List;
 import java.util.Scanner;
 
 public class ConexionDemo {
+    static Scanner scanner = new Scanner(System.in);
      // Insrtanciar Scanner
     static Connection conexion = null;
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
         String url = "jdbc:mysql://localhost:3306/pageturner";
         String usuario = "root";
         String pwd = "victor18";
+        Libro libro = null;
+        String isbn;
+
         int opc = -1;
         do {
             MostrarMenu();
             System.out.print("Seleccione una opcion del menu: ");
             opc = scanner.nextInt();
+            scanner.nextLine(); // consume \n
             switch (opc) {
                 case 1:
                     VerificarConexion(url, usuario, pwd);
                     break;
-                case 2:
-                    InsertarLibro();
-                case 3: 
+                case 2: // Create
+                    libro = CapturarDatosUsuario();
+                    InsertarLibro(libro);
+                    break;
+                case 3: // Read
                     ListarLibros();
+                    break;
+                case 4: // Update
+                    isbn = CapturarISBN();
+                    libro = CapturarDatosUsuario();
+                    ActualizarLibro(isbn, libro);
+                    break;
+                case 5:
+                    isbn = CapturarISBN();
+                    EliminarLibro(isbn);
+                    break;
                 default:
                     break;
             }
         }while(opc != -1);
-        
+        scanner.close();
     }
 
     public static void MostrarMenu(){
@@ -37,7 +53,34 @@ public class ConexionDemo {
         System.out.println("1. Verificar Conexion");
         System.out.println("2. Insertar Libro ");
         System.out.println("3. Leer Libros ");
+        System.out.println("4. Actualizar Libro");
+        System.out.println("5. Eliminar Libro");
         System.out.println("-1. Salir del Sistema ");
+    }
+
+    public static Libro CapturarDatosUsuario(){
+        Libro libroACrear = new Libro();
+        // Capturar datos del usuario
+        System.out.println("Ingrese el ISBN del libro: ");
+        libroACrear.setIsbn(scanner.nextLine());
+        System.out.println("Ingrese el Titulo del libro: ");
+        libroACrear.setTitulo(scanner.nextLine());
+        System.out.println("Ingrese el Autor del libro: ");
+        libroACrear.setAutor(scanner.nextLine());
+        System.out.println("Ingrese el Precio del libro: ");
+        libroACrear.setPrecio(scanner.nextDouble());
+        scanner.nextLine(); // Limpiar buffer
+        System.out.println("Ingrese el Stock del libro: ");
+        libroACrear.setStock(scanner.nextInt());
+        scanner.nextLine(); // Limpiar buffer
+        return libroACrear;
+    }
+
+    public static String CapturarISBN(){
+        ListarLibros(); // Listar para que el usuario vea que informacion actualizar
+        System.out.println("Ingrese isbn del libro a modificar");
+        String isbn = scanner.nextLine();
+        return isbn;
     }
 
     public static void VerificarConexion(String url, String usuario, String pwd){
@@ -50,24 +93,9 @@ public class ConexionDemo {
         }
     }
 
-    public static boolean InsertarLibro(){
-        Scanner scanner = new Scanner(System.in);
-        // Crear el objeto libro vacio
-        Libro libro = new Libro();
-        System.out.println("====== Agregar Libro: =======");
-        System.out.println("");
-        // Capturar datos del usuario
-        System.out.println("Ingrese el ISBN del libro: ");
-        libro.setIsbn(scanner.nextLine());
-        System.out.println("Ingrese el Titulo del libro: ");
-        libro.setTitulo(scanner.nextLine());
-        System.out.println("Ingrese el Autor del libro: ");
-        libro.setAutor(scanner.nextLine());
-        System.out.println("Ingrese el Precio del libro: ");
-        libro.setPrecio(scanner.nextDouble());
-        System.out.println("Ingrese el Stock del libro: ");
-        libro.setStock(scanner.nextInt());
+    public static boolean InsertarLibro(Libro libro){
         // Insertar el libro en la DB
+        System.out.println("\n====== Agregar Libro: =======");
         // preparar el query
         String query = 
             "INSERT INTO libros(isbn, titulo, autor, precio, stock) " +
@@ -83,13 +111,11 @@ public class ConexionDemo {
             int filas = ps.executeUpdate(); // Ejecutar INSERT, UPDATE o DELETE
             System.out.println("(" + filas + ")" + " filas insertadas");
             if(filas > 0){
-                scanner.close();
                 return true;
             }
         }catch(SQLException ex){
             System.out.println("Hubo un error al agregar el libro: " + ex.getMessage());
         }
-        scanner.close();
         return false;
     }
 
@@ -110,12 +136,55 @@ public class ConexionDemo {
                 libro.setStock(rs.getInt("stock"));
                 ListaLibros.add(libro);
             }
+            // Mostrar el output como tabla
+            String formato = "| %-13s | %-40s | %-20s | %-6s | %-6s |%n";
+            System.out.println("\nLibros Registrados en el Sistema");
+            System.out.println("-".repeat(100));
+            System.out.printf(formato, "ISBN", "TITULO", "AUTOR", "PRECIO", "STOCK");
+            System.out.println("-".repeat(100));
             // Mostramos los resultado
             for(Libro l : ListaLibros){
-                System.out.println(l.getIsbn() + " " + l.getTitulo());
+                System.out.printf(formato, l.getIsbn(), l.getTitulo(), l.getAutor(), l.getPrecio(), l.getStock());
             }
+            System.out.println("-".repeat(100));
         }catch(SQLException ex){
             System.out.println("Ha ocurrido un error al leer los libros. " + ex.getMessage());
         }
+    }
+
+    public static boolean ActualizarLibro(String isbn, Libro libro){
+        System.out.println("\n Actualizar Informacion del Libro: ");
+        int filas = -1;
+        String query = 
+            "UPDATE libros SET titulo = ?, autor=?, precio =?, stock=? " + 
+            "WHERE isbn = ?";
+        try(PreparedStatement ps = conexion.prepareStatement(query)){
+            // Colocar los argumentos reemplazamos ? x valor
+            ps.setString(1, libro.getTitulo());
+            ps.setString(2, libro.getAutor());
+            ps.setDouble(3, libro.getPrecio());
+            ps.setInt(4, libro.getStock());
+            ps.setString(5, isbn);
+            filas = ps.executeUpdate();
+        }catch(SQLException ex){
+            System.out.println("Ha ocurrido un error al actualizar el libro. " + ex.getMessage());
+        }
+        return filas > 0;
+    }
+
+    public static boolean EliminarLibro(String isbn){
+        System.out.println("\n Eliminar Libro: ");
+        int filas = -1;
+        String query = "DELETE FROM libros WHERE isbn = ?";
+        try(PreparedStatement ps = conexion.prepareStatement(query)){
+            ps.setString(1, isbn);
+            filas = ps.executeUpdate();
+            if(filas == 0){
+                System.out.println("ISBN no encontrado: " + isbn);
+            }
+        }catch(SQLException ex){
+            System.out.println("Ha ocurrido un error al eliminar el libro. " + ex.getMessage());
+        }
+        return filas > 0;
     }
 }
